6915aaef6777ecb1fe6b9c0a18c633d9f0e196e4
Diego Cirilo <dvcirilo@gmail.com>
2024-09-01 20:30:39 -0300

    mudando de var para const

--- settings.sample.js
+++ settings.sample.js
@@ -1,4 +1,4 @@
-var CLIENT_ID = '';
-var REDIRECT_URI = 'http://localhost:8888/';
-var SUAP_URL = 'https://suap.ifrn.edu.br';
-var SCOPE = 'identificacao email documentos_pessoais';
+const CLIENT_ID = '';
+const REDIRECT_URI = 'http://localhost:8888/';
+const SUAP_URL = 'https://suap.ifrn.edu.br';
+const SCOPE = 'identificacao email documentos_pessoais';
